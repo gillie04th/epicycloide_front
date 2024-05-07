c74ccedbd32690b64a99999108fef52576c51abf
matt�o richefeu <jean44774@gmail.com>
2024-05-07 18:25:29 +0200

    creation composant button Icon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {ButtonComponent} from "./components/button/button.component";
 import {ButtonModule} from "primeng/button";
+import {ButtonWithIconComponent} from "./components/button-with-icon/button-with-icon.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, ButtonModule],
+  imports: [RouterOutlet, ButtonComponent, ButtonModule, ButtonWithIconComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/components/button-with-icon/button-with-icon.component.ts
+++ src/app/components/button-with-icon/button-with-icon.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {ButtonModule} from "primeng/button";
+
 
 @Component({
-  selector: 'app-button-with-icon',
+  selector: 'button-icon',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonModule
+  ],
   templateUrl: './button-with-icon.component.html',
   styleUrl: './button-with-icon.component.css'
 })
 export class ButtonWithIconComponent {
-
+  onClickHandler() {
+    console.log('Button Icon clicked');
+  }
 }
