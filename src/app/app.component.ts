d137088b35c8af3882290f0a822a7eccd08a8106
matt�o richefeu <jean44774@gmail.com>
2024-05-07 16:17:20 +0200

    creation composant button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonComponent} from "./components/button/button.component";
+import {ButtonModule} from "primeng/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent, ButtonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Front';
 }
---
+++ src/app/components/button-with-icon/button-with-icon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-with-icon',
+  standalone: true,
+  imports: [],
+  templateUrl: './button-with-icon.component.html',
+  styleUrl: './button-with-icon.component.css'
+})
+export class ButtonWithIconComponent {
+
+}
