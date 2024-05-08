322fa16ca5cbe7869f4d1bebac318f94120e9e0b
matt�o richefeu <jean44774@gmail.com>
2024-05-08 11:03:40 +0200

    creation composant hearder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {ButtonComponent} from "./components/button/button.component";
 import {ButtonModule} from "primeng/button";
 import {ButtonWithIconComponent} from "./components/button-with-icon/button-with-icon.component";
+import {MyComponentModule} from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, ButtonModule, ButtonWithIconComponent],
+  imports: [RouterOutlet, ButtonComponent, ButtonModule, ButtonWithIconComponent, MyComponentModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
