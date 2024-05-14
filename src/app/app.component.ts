70f2a5900aaf12223e3f320202d7f9b8ca78e96e
theophile <theophile.gillier@gmail.com>
2024-05-14 15:24:26 +0200

    composant chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import {ButtonModule} from "primeng/button";
 import {ButtonWithIconComponent} from "./components/button-with-icon/button-with-icon.component";
 import {MyComponentModule} from "./components/header/header.component";
 import {SlideBarComponent} from "./components/slide-bar/slide-bar.component";
+import {ChartComponent} from "./components/chart/chart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonComponent, ButtonModule, ButtonWithIconComponent, MyComponentModule, SlideBarComponent],
+  imports: [RouterOutlet, ButtonComponent, ButtonModule, ButtonWithIconComponent, MyComponentModule, SlideBarComponent, ChartComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
